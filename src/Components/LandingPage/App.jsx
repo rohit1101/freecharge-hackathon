7694cf187eeb417c2b15629b9bd5f2d60459d072
rohit1101 <srohit1101@gmail.com>
2020-10-13 16:50:08 +0530

    Final style commit

--- src/Components/LandingPage/App.jsx
+++ src/Components/LandingPage/App.jsx
@@ -1,7 +1,6 @@
 import { Link } from '@reach/router';
 import React from 'react';
-
-
+import styles from './App.module.css'
 
 export default class App extends React.Component {
   state={
@@ -17,23 +16,26 @@ export default class App extends React.Component {
   
   render() {
     return (
-      <div>
+      <div className={styles.container}>
         <h1>Recipes</h1>
+        <div className={styles.card_container}>
         {this.state.recipe_arr.map(recipe=> {
           return (
-            <div key={recipe.id}>
+            <div key={recipe.id} className={styles.card}>
+            
                 <h2>{recipe.name}</h2>
                 <h3>{recipe.category}</h3>
                 <h4>${recipe.price}</h4>
-                <img src={recipe.image} alt={recipe.name} />
+                <img width="200px" height="150px" src={recipe.image} alt={recipe.name} />
                 <p>{recipe.description}</p>
                 <h5>Label: {recipe.label ? recipe.label : 'Random' }</h5>
-                
                 <Link to={`/payment/${recipe.id}`}>Pay Now</Link>
-                <hr/>
             </div>
+            
           )
         })}
+        </div>
+
       </div>
     )
   }
